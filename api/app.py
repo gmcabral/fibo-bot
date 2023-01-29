59eff7ba7086b74106c4ecf509c42072e95bde8e
Gustavo Cabral <gmcabral@gmail.com>
2023-01-29 19:51:00 -0300

    change app.py to use 0.0.0.0

--- api/app.py
+++ api/app.py
@@ -8,3 +8,6 @@ app.register_blueprint(users_blueprint)
 app.register_blueprint(fibos_blueprint)
 
 # app.register_blueprint(items_blueprint)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
